2546c9b9095b940d12c84f388b69f34300f128f7
DESKTOP-FVNSORI\unsto <willisac@etsu.edu>
2018-11-10 21:12:43 -0500

    Unpacked zip file

---
+++ Assets/Scripts/Health.cs
@@ -0,0 +1,19 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Health : MonoBehaviour
+{
+
+    // Current Health
+    [SerializeField]
+    int cur = 5;
+
+    public void damage(int n)
+    {
+        cur -= n;
+
+        if (cur <= 0)
+            Object.Destroy(gameObject);
+    }
+}
